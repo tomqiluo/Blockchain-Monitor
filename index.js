5de5849633c1f9a44942632ff0817f945161862a
tomqiluo <tomqiluo@bu.edu>
2023-05-03 14:21:52 -0400

    Pinging ESP32

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+// node index.js
+// de1247bd2ff24166a1c4622def323c50
+
+
 const https = require('https');
 const readline = require('readline');
+const ping = require('ping');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+async function pingESP32() {
+  const subnet = '192.168.1';
+  const esp32Identifier = 'Espressif';
+
+  for (let i = 1; i <= 254; i++) {
+    const ip = `${subnet}.${i}`;
+    const res = await ping.promise.probe(ip, { timeout: 2 });
+
+    if (res.alive && res.host && res.host.startsWith(esp32Identifier)) {
+      console.log(`ESP32 found at IP address: ${ip}`);
+      break;
+    }
+  }
+}
 
 rl.question('Please enter the Infura API key: ', (apiKey) => {
   const data = JSON.stringify({
@@ -40,6 +60,8 @@ rl.question('Please enter the Infura API key: ', (apiKey) => {
 
   req.write(data)
   req.end()
+
+  pingESP32(); // Pinging ESP32
 
   rl.close();
 });
