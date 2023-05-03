883d30203dff94af7568730d02331b792a9cd3fc
tomqiluo <tomqiluo@bu.edu>
2023-05-03 01:07:06 -0400

    Read the latest ethereum Block

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const https = require('https');
-const projectId = 'de1247bd2ff24166a1c4622def323c50';
-const data = JSON.stringify({
-    "jsonrpc":"2.0","method":"eth_blockNumber","params": [],"id":1
-  })
-const options = {
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Please enter the Infura API key: ', (apiKey) => {
+  const data = JSON.stringify({
+    "jsonrpc": "2.0",
+    "method": "eth_blockNumber",
+    "params": [],
+    "id": 1
+  });
+
+  const options = {
     host: 'mainnet.infura.io',
     port: 443,
-    path: '/v3/' + projectId,
+    path: '/v3/' + apiKey,
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json'
-      },
-};
-const req = https.request(options, res => {
+      'Content-Type': 'application/json'
+    },
+  };
+
+  const req = https.request(options, res => {
     console.log(`statusCode: ${res.statusCode}`)
 
     res.on('data', d => {
-      process.stdout.write(d)
+      const response = JSON.parse(d);
+      const blockNumber = parseInt(response.result, 16);
+      console.log(`Block number: ${blockNumber}`);
     })
   })
 
@@ -26,3 +40,6 @@ const req = https.request(options, res => {
 
   req.write(data)
   req.end()
+
+  rl.close();
+});
